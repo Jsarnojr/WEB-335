fe0e0b8730809e98a91574bd7eabf4919a731529
Joseph Sarno Jr <jsarnojr9690@gmail.com>
2024-08-24 13:25:37 -0400

    Add files via upload

---
+++ quote.py
@@ -0,0 +1,9 @@
+"""
+Author: [Your Name]
+Date: [Current Date]
+File Name: quote.py
+Description: This program prints a quote and gives credit to the original author.
+"""
+
+# Printing a famous quote by Albert Einstein
+print("Life is like riding a bicycle. To keep your balance, you must keep moving. - Albert Einstein")
